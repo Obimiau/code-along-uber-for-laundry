774715d51166df293e2f4b2f452f87171dc5d9f5
Maria Ruiz <mariaruizestevez@gmail.com>
2017-11-17 12:21:04 +0100

    Fifth iteration done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,15 +1,26 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
+const authRoute = require('./routes/auth');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const app = express();
+const laundryRoutes = require('./routes/laundry');
+const dbURL = 'mongodb://localhost/uberlaundry';
+mongoose.connect(dbURL).then( () => {
+  debug(`Connected to ${dbURL}`);
+});
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,12 +37,54 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'never do your own laundry again',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'never do your own laundry again',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+app.use((req, res, next) => {
+  if (req.session.currentUser) {
+    res.locals.currentUserInfo = req.session.currentUser;
+    res.locals.isUserLoggedIn = true;
+  } else {
+    res.locals.isUserLoggedIn = false;
+  }
+
+  next();
+});
+
+app.use('/', index);
+
 app.use('/', index);
 app.use('/users', users);
+app.use('/', authRoute);
+app.use('/', laundryRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
---
+++ starter-code/routes/laundry.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const User = require('../models/user');
+const router = express.Router();
+
+
+router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+    return;
+  }
+
+  res.redirect('/login');
+});
+
+
+router.get('/dashboard', (req, res, next) => {
+  res.render('laundry/dashboard');
+});
+
+router.post('/launderers', (req, res, next) => {
+  const userId = req.session.currentUser._id;
+  const laundererInfo = {
+    fee: req.body.fee,
+    isLaunderer: true
+  };
+
+  User.findByIdAndUpdate(userId, laundererInfo, { new: true }, (err, theUser) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    req.session.currentUser = theUser;
+
+    res.redirect('/dashboard');
+  });
+});
+
+router.get('/launderers', (req, res, next) => {
+  User.find({ isLaunderer: true }, (err, launderersList) => {
+   if (err) {
+     next(err);
+     return;
+   }
+
+   res.render('laundry/launderers', {
+     launderers: launderersList
+   });
+  });
+});
+
+module.exports = router;
